e6383dacbaf0e002e40e70844bf1d01bf810c10c
Denomparkour <42404684+denomparkour@users.noreply.github.com>
2020-04-30 09:18:25 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,20 +20,18 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! If you have any questions on how to use me, read /help.
-
-I'm a group manager bot For AtomOS community and my lead dev is [Naveen Achari](t.me/thephotogenicbug)
-
+I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
+you can find what makes me tick [here](github.com/denomparkour/parkourxbot)!
+Feel free to submit pull requests on github
 This bot is maintained by the [nub guy](t.me/thedenomparkour)
-
 You can find the list of available commands with /help.
-
-Made with love for [Atom Community ❤️](t.me/atomosofficial)
+If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """
+
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
-
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
@@ -42,7 +40,6 @@ the things I can help you with.
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
-
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
