2779e60d7f7f55eed8f7f1d55b07f268223c8b5e
Denomparkour <42404684+denomparkour@users.noreply.github.com>
2020-05-24 22:10:31 +0530

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -114,7 +114,7 @@ def info(bot: Bot, update: Update, args: List[str]):
                 text += tld(chat.id, "misc_info_is_whitelisted")
 
     for mod in USER_INFO:
-        mod_info = mod.__user_info__(user.id, chat_10).strip()
+        mod_info = mod.__user_info__(user.id, chat_id).strip()
         if mod_info:
             text += "\n\n" + mod_info
 
